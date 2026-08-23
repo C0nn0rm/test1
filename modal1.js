0310c4ed03239a96abfb664c9a4cd2e7205328c2
C0nn0rm <129839677+C0nn0rm@users.noreply.github.com>
2026-08-23 17:53:17 +0100

    Update modal1.js

--- modal1.js
+++ modal1.js
@@ -24,3 +24,48 @@ modal.onclick = function(event) {
         modal.style.display = "none";
     }
 };
+
+var navbar = document.getElementById("navbar");
+
+img.onclick = function() {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+    navbar.style.display = "none";
+};
+
+span.onclick = function() {
+    modal.style.display = "none";
+    navbar.style.display = "";
+};
+
+var navbar = document.getElementById("navbar");
+
+img.onclick = function() {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+
+    navbar.classList.add("nav-hidden");
+};
+
+span.onclick = function() {
+    modal.style.display = "none";
+
+    navbar.classList.remove("nav-hidden");
+};
+
+var header = document.getElementById("site-header");
+
+img.onclick = function() {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+
+    header.classList.add("nav-hidden");
+};
+
+span.onclick = function() {
+    modal.style.display = "none";
+    header.classList.remove("nav-hidden");
+};
